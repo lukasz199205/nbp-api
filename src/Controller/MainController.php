a725cfd7466607870e9d2cf5b69d6e764b55068d
Łukasz Mentel <lukasz199205@gmail.com>
2021-11-08 13:12:18 +0100

    zakutalizowanie controllera, by korzystał z serwisu do akutalizacji walut

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Currency;
+use App\Service\CurrencyUpdate;
 use App\Service\NBPApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,33 +14,11 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main")
      */
-    public function index(NBPApiClient $client): Response
+    public function index(NBPApiClient $client, CurrencyUpdate $update): Response
     {
         $em = $this->getDoctrine()->getManager();
-
         $rates = $client->fetchCurrenciesFromNBP();
-
-        //iteracja po pobranych danych
-        foreach ($rates[0]['rates'] as $rate) {
-            // pobranie aktualnych danych z bazy dla konkretnej waluty
-            $currency = $em->getRepository(Currency::class)
-                ->findOneBy(['name' => $rate['currency']]);
-
-            // jeżeli waluta nie istnieje w bazie to ją utwórz
-            if (!$currency) {
-                $currency = new Currency();
-                $currency->setName($rate['currency']);
-                $currency->setCurrencyCode($rate['code']);
-            }
-
-            // jeżeli kurs jest różny od tego zapisanego w bazie to aktualizuj
-            if ($currency->getExchangeRate() != $rate['mid']) {
-                $currency->setExchangeRate($rate['mid']);
-            }
-
-            $em->persist($currency);
-        }
-        $em->flush();
+        $update->update($rates);
 
         //pobranie danych z bazy do wyświetlenia
         $data = $em->getRepository(Currency::class)
