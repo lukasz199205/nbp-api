76796a713f073d7d726c9d596873e53b9d9afcc1
Łukasz Mentel <lukasz199205@gmail.com>
2021-11-08 13:33:24 +0100

    dostosowanie prezycji typu danych dla kursów walut

--- migrations/Version20211108095408.php
+++ migrations/Version20211108095408.php
@@ -20,7 +20,7 @@ final class Version20211108095408 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE currency (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', name VARCHAR(100) NOT NULL, currency_code VARCHAR(3) NOT NULL, exchange_rate NUMERIC(12, 10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE currency (id BINARY(16) NOT NULL COMMENT \'(DC2Type:uuid)\', name VARCHAR(100) NOT NULL, currency_code VARCHAR(3) NOT NULL, exchange_rate NUMERIC(10, 9) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
