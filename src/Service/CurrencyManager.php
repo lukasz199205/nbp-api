1573e1528052ef77c0b1397ef27abfae64e7508b
Łukasz Mentel <lukasz199205@gmail.com>
2021-11-24 13:05:26 +0100

    usprawnienie działania serwisu
    -zmniejszenie liczby zapytań do bazy poprzez zastosowanie operacji na tablicach
    - podział na funkcje update i create

--- src/Service/CurrencyManager.php
+++ src/Service/CurrencyManager.php
@@ -15,28 +15,37 @@ class CurrencyManager
         $this->em = $em;
     }
 
-    //TODO podzielić funkcje na update i create oraz dodatkowo pozbyć się z pętli zapytania do bazy
-    public function update(array $rates) :void
+    /**
+     * @param array $rates
+     * @return array
+     */
+    public function checkForCurrencyUpdate(array $rates) :array
     {
+        $currencies = $this->em->getRepository(Currency::class)
+            ->findBy([],['name' => 'ASC']);
+
         foreach ($rates as $rate) {
-
-            // pobranie aktualnych danych z bazy dla konkretnej waluty
-            $currency = $this->em->getRepository(Currency::class)
-                ->findOneBy(['name' => $rate['currency']]);
-
-            // jeżeli waluta nie istnieje w bazie to ją utwórz
-            if (!$currency) {
-                $currency = new Currency();
-                $currency->setName($rate['currency']);
-                $currency->setCurrencyCode($rate['code']);
+            $index = array_search($rate['code'], array_column($currencies, 'currency_code'));
+            if (($index !== false) && ($rate['mid'] != $currencies[$index]->getExchangeRate())) {
+                $currencies[$index]->setExchangeRate($rate['mid']);
+            } elseif ($index === false) {
+                $this->createNewCurrency($rate);
             }
-
-            // jeżeli kurs jest różny od tego zapisanego w bazie to aktualizuj
-            if ($currency->getExchangeRate() != $rate['mid']) {
-                $currency->setExchangeRate($rate['mid']);
-            }
-            $this->em->persist($currency);
         }
         $this->em->flush();
+        dump($currencies);
+        return $currencies;
+    }
+
+    /**
+     * @param array $rate
+     */
+    public function createNewCurrency(array $rate) :void
+    {
+        $currency = new Currency();
+        $currency->setName($rate['currency']);
+        $currency->setCurrencyCode($rate['code']);
+        $currency->setExchangeRate($rate['mid']);
+        $this->em->persist($currency);
     }
 }
